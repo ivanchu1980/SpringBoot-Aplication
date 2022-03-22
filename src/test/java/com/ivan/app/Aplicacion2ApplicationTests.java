c4ddb6feab1204fdc28dcaf2da4a3e05919d32b3
Ivanchu <ivanchu1980@hotmail.com>
2022-03-23 00:56:23 +0100

    Commit inicial

---
+++ src/test/java/com/ivan/app/Aplicacion2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aplicacion2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
